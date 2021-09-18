6e40014312e8c2535e738fcad8acded36f5a7a1f
Patipan Buranangura <patipan.b7@gmail.com>
2021-09-19 03:44:06 +0700

    first commit

---
+++ routes/items.js
@@ -0,0 +1,95 @@
+const items = require("../Items");
+
+const {
+  getItems,
+  getItem,
+  addItem,
+  deleteItem,
+  updateItem,
+} = require("../controller/items");
+// Items schema
+const Item = {
+  type: "object",
+  properties: {
+    id: {type: "string"},
+    name: {type: "string"},
+  },
+};
+
+//Options for get all items
+const getItemsOpts = {
+  schema: {
+    response: {
+      200: {
+        type: "array",
+        items: Item,
+      },
+    },
+  },
+  handler: getItems,
+};
+
+const getItemOpts = {
+  schema: {
+    response: {
+      200: Item,
+    },
+  },
+  handler: getItem,
+};
+
+const postItemOpts = {
+  schema: {
+    body: {
+      type: "object",
+      required: ["name"],
+      properties: {
+        name: {type: "string"},
+      },
+    },
+    response: {
+      201: Item,
+    },
+  },
+  handler: addItem,
+};
+
+const deleteItemOpts = {
+  schema: {
+    response: {
+      200: {
+        type: "object",
+        properties: {
+          message: {type: "string"},
+        },
+      },
+    },
+  },
+  handler: deleteItem,
+};
+
+const updateItemOpts = {
+  schema: {
+    response: {
+      200: Item,
+    },
+  },
+  handler: updateItem,
+};
+
+function itemRoutes(fastify, options, done) {
+  // GET all items
+  fastify.get("/items", getItemsOpts);
+  // GET single item
+  fastify.get("/items/:id", getItemOpts);
+  // Add item
+  fastify.post("/items", postItemOpts);
+  // Delete item
+  fastify.delete("/items/:id", deleteItemOpts);
+  // Update item
+  fastify.put("/items/:id", updateItemOpts);
+
+  done();
+}
+
+module.exports = itemRoutes;
